dc636a6a6a47f4b090d5d9e19393d101a0ebe44e
Tiff Chan <tiffchan@Tiffanys-MacBook-Air.local>
2024-01-07 22:46:24 -0500

    Corrected schema tables

--- db/schema.sql
+++ db/schema.sql
@@ -11,6 +11,7 @@ CREATE TABLE department (
 CREATE TABLE role (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   title VARCHAR(30),
+  department_id INT,
   salary DECIMAL,
   FOREIGN KEY (department_id)
   REFERENCES department(id)
@@ -21,6 +22,8 @@ CREATE TABLE employee (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   first_name VARCHAR(30),
   last_name DECIMAL,
+  role_id INT, 
+  manager_id INT NULL,
   FOREIGN KEY (role_id)
   REFERENCES role(id)
   ON DELETE SET NULL,
