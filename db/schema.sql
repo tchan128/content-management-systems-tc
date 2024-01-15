b0832cbc09f5824a0db5e48f127eb29756bf59fa
Tiff Chan <tiffchan@Tiffanys-MacBook-Air.local>
2024-01-14 20:21:58 -0500

    Modified schema.sql and seeds.sql

--- db/schema.sql
+++ db/schema.sql
@@ -4,30 +4,27 @@ CREATE DATABASE employee_db;
 USE employee_db;
 
 CREATE TABLE department (
-  id INT AUTO_INCREMENT PRIMARY KEY,
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   name VARCHAR(30)
 );
 
 CREATE TABLE role (
-  id INT AUTO_INCREMENT PRIMARY KEY,
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   title VARCHAR(30),
   department_id INT,
   salary DECIMAL,
   FOREIGN KEY (department_id)
   REFERENCES department(id)
-  ON DELETE SET NULL
 );
 
 CREATE TABLE employee (
-  id INT AUTO_INCREMENT PRIMARY KEY,
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   first_name VARCHAR(30),
   last_name DECIMAL,
   role_id INT, 
   manager_id INT NULL,
   FOREIGN KEY (role_id)
-  REFERENCES role(id)
-  ON DELETE SET NULL,
+  REFERENCES role(id),
   FOREIGN KEY (manager_id)
   REFERENCES employee(id)
-  ON DELETE SET NULL
 );
